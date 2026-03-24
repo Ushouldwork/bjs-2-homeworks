144bf527a6483c2ce2ffd07833f6f2e1e382b370
GODPLEASEENO <xoxlova2016@bk.ru>
2026-03-24 22:56:51 +0300

    итог второй домашки

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -6,8 +6,14 @@ function getArrayParams(...arr) {
   let sum = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) min = arr[i];
-    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
     sum += arr[i];
   }
 
@@ -15,76 +21,6 @@ function getArrayParams(...arr) {
 
   return { min, max, avg };
 }
-
-function summElementsWorker(...arr) {
-  let sum = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-
-  return sum;
-}
-
-function differenceMaxMinWorker(...arr) {
-  if (arr.length === 0) return 0;
-
-  let min = arr[0];
-  let max = arr[0];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) min = arr[i];
-    if (arr[i] > max) max = arr[i];
-  }
-
-  return max - min;
-}
-
-function differenceEvenOddWorker(...arr) {
-  let evenSum = 0;
-  let oddSum = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      evenSum += arr[i];
-    } else {
-      oddSum += arr[i];
-    }
-  }
-
-  return evenSum - oddSum;
-}
-
-function averageEvenElementsWorker(...arr) {
-  let sum = 0;
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      sum += arr[i];
-      count++;
-    }
-  }
-
-  if (count === 0) return 0;
-
-  return sum / count;
-}
-
-function makeWork(arrOfArr, func) {
-  let maxResult = -Infinity;
-
-  for (let i = 0; i < arrOfArr.length; i++) {
-    let result = func(...arrOfArr[i]);
-
-    if (result > maxResult) {
-      maxResult = result;
-    }
-  }
-
-  return maxResult;
-}
-
 
 function summElementsWorker(...arr) {
   if (arr.length === 0) {
@@ -161,8 +97,6 @@ function averageEvenElementsWorker(...arr) {
 
   return sumEvenElement / countEvenElement;
 }
-
-
 
 function makeWork(arrOfArr, func) {
   let maxWorkerResult = -Infinity;
