8d0ca98bb714ceb09c160d22d716f527b8eb16d6
GODPLEASEENO <xoxlova2016@bk.ru>
2026-03-24 18:46:09 +0300

    commplite ит

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
+  
+  let d = b * b - 4 * a * c;
+  
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
   
   return arr;
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = percent / 100 / 12;
+  let S = amount - contribution;
+
+  if (S <= 0) {
+    return 0;
+  }
+
+  let monthlyPayment = S * (P + (P / ((1 + P) ** countMonths - 1)));
+  let total = monthlyPayment * countMonths;
+
+  return Math.round(total * 100) / 100;
 }
